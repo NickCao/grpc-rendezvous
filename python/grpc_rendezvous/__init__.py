8078cac86fcb4f377b9379e57620757a6087005a
Nick Cao <nickcao@nichi.co>
2024-06-18 14:20:30 -0400

    implement a full python server

--- python/grpc_rendezvous/__init__.py
+++ python/grpc_rendezvous/__init__.py
@@ -3,14 +3,31 @@ import grpc
 
 from jumpstarter.v1 import rendezvous_pb2
 from jumpstarter.v1 import rendezvous_pb2_grpc
+from jumpstarter.v1 import jumpstarter_pb2
+from jumpstarter.v1 import jumpstarter_pb2_grpc
+
+
+class ForClientServicer(jumpstarter_pb2_grpc.ForClientServicer):
+    async def GetReport(self, request, context):
+        context.set_details("dummy implementation in python")
+        context.set_code(grpc.StatusCode.INTERNAL)
 
 
 async def main():
+    server = grpc.aio.server()
+
+    jumpstarter_pb2_grpc.add_ForClientServicer_to_server(ForClientServicer(), server)
+
+    # grpc.aio only supports TCP
+    server.add_insecure_port("127.0.0.1:8002")
+
+    asyncio.create_task(server.start())
+
     async with grpc.aio.insecure_channel("127.0.0.1:8000") as channel:
         rendezvous = rendezvous_pb2_grpc.RendezvousServiceStub(channel)
 
         async def handle(resp):
-            reader, writer = await asyncio.open_connection("127.0.0.1", 5201)
+            reader, writer = await asyncio.open_connection("127.0.0.1", 8002)
 
             fqueue = asyncio.Queue()
 
