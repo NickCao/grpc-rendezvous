6fdd7e91e99049081543de426a7358f9b8a90510
Nick Cao <nickcao@nichi.co>
2024-06-18 09:49:14 -0400

    implement Listen and Dial

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/NickCao/grpc-rendezvous
 go 1.22.3
 
 require (
+	github.com/google/uuid v1.6.0
 	golang.org/x/sync v0.6.0
 	google.golang.org/grpc v1.64.0
 	google.golang.org/protobuf v1.34.2
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -6,6 +6,7 @@ import (
 
 	st "github.com/NickCao/grpc-rendezvous/pkg/stream"
 	pb "github.com/NickCao/grpc-rendezvous/proto"
+	"github.com/google/uuid"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
@@ -13,12 +14,55 @@ import (
 
 type RendezvousServer struct {
 	pb.UnimplementedRendezvousServer
+	listenMap *sync.Map
 	streamMap *sync.Map
+}
+
+type listenCtx struct {
+	cancel context.CancelFunc
+	stream pb.Rendezvous_ListenServer
 }
 
 type streamCtx struct {
 	cancel context.CancelFunc
 	stream pb.Rendezvous_StreamServer
+}
+
+func (s *RendezvousServer) Listen(req *pb.Request, stream pb.Rendezvous_ListenServer) error {
+	ctx, cancel := context.WithCancel(stream.Context())
+
+	cond := listenCtx{
+		cancel: cancel,
+		stream: stream,
+	}
+
+	actual, loaded := s.listenMap.Swap(req.Address, cond)
+	if loaded {
+		actual.(listenCtx).cancel()
+	}
+
+	select {
+	case <-ctx.Done():
+		return nil
+	}
+}
+
+func (s *RendezvousServer) Dial(ctx context.Context, req *pb.Request) (*pb.Response, error) {
+	value, ok := s.listenMap.Load(req.Address)
+	if !ok {
+		return nil, status.Errorf(codes.Unavailable, "no matching listener")
+	}
+
+	resp := &pb.Response{
+		Stream: uuid.New().String(),
+	}
+
+	err := value.(listenCtx).stream.Send(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
 }
 
 func (s *RendezvousServer) Stream(stream pb.Rendezvous_StreamServer) error {
