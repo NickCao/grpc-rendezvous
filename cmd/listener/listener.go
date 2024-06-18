702e1e04371fc17b290763237fc726cca7b471fa
Nick Cao <nickcao@nichi.co>
2024-06-18 09:59:31 -0400

    create listener

--- cmd/listener/listener.go
+++ cmd/listener/listener.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
 
+	pb "github.com/NickCao/grpc-rendezvous/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
+)
+
+func main() {
+	c, err := grpc.NewClient("127.0.0.1:8000",
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := pb.NewRendezvousClient(c)
+
+	listen, err := client.Listen(context.TODO(), &pb.Request{
+		Address: "dummy",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listen.Recv()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ctx := metadata.NewOutgoingContext(context.TODO(), metadata.Pairs("stream", conn.Stream))
+
+		_, err = client.Stream(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
